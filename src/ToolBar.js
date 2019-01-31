6ee1fd2a2420acb1560b5ece1f92802fbc65dc07
Jonas Lundberg <jonas@5monkeys.se>
2019-01-31 12:38:32 +0100

    Add justify and override features to ToolBar

--- src/ToolBar.js
+++ src/ToolBar.js
@@ -12,31 +12,74 @@ const styles = theme => ({
     flexGrow: 0,
     flexShrink: 0,
   },
-  bottom: {
-    bottom: 0,
-    top: "auto",
+  toolbar: {},
+  justifyStart: {
+    justifyContent: "flex-start",
   },
-  toolbar: {
-    "& > * + *": {
-      marginLeft: theme.spacing.unit,
+  justifyCenter: {
+    justifyContent: "center",
+  },
+  justifyEnd: {
+    justifyContent: "flex-end",
+  },
+  justifyBetween: {
+    justifyContent: "space-between",
+  },
+  justifyAround: {
+    justifyContent: "space-around",
+  },
+  justifyEvenly: {
+    justifyContent: "space-evenly",
+  },
+  borderTop: {},
+  borderBottom: {},
+  colorPaper: {
+    backgroundColor: theme.palette.background.paper,
+    color: theme.palette.getContrastText(theme.palette.background.paper),
+    "& $borderTop": {
+      borderTop: "1px solid rgba(0, 0, 0, 0.14)",
+    },
+    "& $borderBottom": {
+      borderBottom: "1px solid rgba(0, 0, 0, 0.14)",
     },
   },
 });
 
-const ToolBar = ({ classes, children, placement, color, ...rest }) => {
+const ToolBar = ({
+  classes,
+  overrides,
+  children,
+  placement,
+  color,
+  border,
+  dense,
+  justify,
+  ...rest
+}) => {
   return (
     <AppBar
       elevation={0}
       position="relative"
-      color={color}
+      color={color !== "paper" ? color : undefined}
       className={classNames(classes.root, {
-        [classes.bottom]: placement === "bottom",
+        [classes.colorPaper]: color === "paper",
       })}
       {...rest}
     >
       <Container>
         <Toolbar
-          className={classes.toolbar}
+          variant={dense ? "dense" : "regular"}
+          className={classNames(classes.toolbar, {
+            [overrides.toolbar]: overrides.toolbar,
+            [classes.justifyStart]: justify === "start",
+            [classes.justifyCenter]: justify === "center",
+            [classes.justifyEnd]: justify === "end",
+            [classes.justifyBetween]: justify === "between",
+            [classes.justifyAround]: justify === "around",
+            [classes.justifyEvenly]: justify === "evenly",
+            [classes.borderTop]: border === "top",
+            [classes.borderBottom]: border === "bottom",
+          })}
           classes={{ gutters: classes.toolbarGutters }}
         >
           {children}
@@ -48,15 +91,21 @@ const ToolBar = ({ classes, children, placement, color, ...rest }) => {
 
 ToolBar.propTypes = {
   classes: PropTypes.object.isRequired,
+  overrides: PropTypes.object,
   children: PropTypes.node,
-  placement: PropTypes.string,
   color: PropTypes.string,
+  border: PropTypes.string,
+  dense: PropTypes.bool,
+  justify: PropTypes.string, // start|center|end|between|around|evenly
 };
 
 ToolBar.defaultProps = {
+  overrides: {},
   children: null,
-  placement: "bottom",
   color: "primary",
+  border: "top",
+  dense: false,
+  justify: "end",
 };
 
 export default withStyles(styles, { name: "BananasToolBar" })(ToolBar);
