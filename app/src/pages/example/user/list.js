267672502b148ef107418818d38cb709729eea68
Jonas Lundberg <jonas@5monkeys.se>
2019-03-13 10:29:08 +0100

    Add error page button to example app user list page

--- app/src/pages/example/user/list.js
+++ app/src/pages/example/user/list.js
@@ -52,212 +52,240 @@ const styles = theme => ({
   },
 });
 
-const UsersPage = ({ route, data, title, logger, classes }) => {
-  const { query, hash } = route;
-  logger.debug("Rendering users list with data:", data);
-  return (
-    <AdminContext.Consumer>
-      {context => (
-        <>
-          <TitleBar title={title} color="primary">
-            <Tools>
-              <Link route="example.user:create">
-                <Fab
+class UsersPage extends React.Component {
+  state = {};
+
+  render() {
+    const { route, data, title, logger, classes } = this.props;
+    const { query, hash } = route;
+    const { throwError } = this.state;
+
+    logger.debug("Rendering users list with data:", data);
+
+    if (throwError) {
+      throw new Error("fail!");
+    }
+
+    return (
+      <AdminContext.Consumer>
+        {context => (
+          <>
+            <TitleBar title={title} color="primary">
+              <Tools>
+                <Link route="example.user:create">
+                  <Fab
+                    color="primary"
+                    size="medium"
+                    variant="extended"
+                    className={classes.addButton}
+                  >
+                    <PersonAddIcon className={classes.addIcon} /> Add
+                  </Fab>
+                </Link>
+                <Link route="example.user:create">
+                  <Fab
+                    color="secondary"
+                    size="medium"
+                    variant="extended"
+                    className={classes.addButton}
+                  >
+                    <PersonAddIcon className={classes.addIcon} /> Add
+                  </Fab>
+                </Link>
+                <IconButton color="inherit">
+                  <Badge badgeContent={4} color="secondary">
+                    <NotificationsIcon />
+                  </Badge>
+                </IconButton>
+              </Tools>
+            </TitleBar>
+            <TitleBar
+              title="Filters"
+              color="paper"
+              border="bottom"
+              dense
+              overrides={{
+                toolbar: classes.evenToolbar,
+              }}
+            >
+              <Input placeholder={"Search..."} />
+              <Tools>
+                <IconButton color="inherit">
+                  <FilterListIcon />
+                </IconButton>
+              </Tools>
+            </TitleBar>
+
+            <Content contained={false}>
+              <Typography>
+                <strong>Route:</strong> {route.id}
+              </Typography>
+              <Typography>
+                <strong>Query:</strong>{" "}
+                {query &&
+                  Object.entries(query).map(
+                    ([key, value]) => `${key} : ${value}, `
+                  )}
+              </Typography>
+              <Typography>
+                <strong>Hash:</strong> {hash}
+              </Typography>
+              <Paper className={classes.users}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>ID</TableCell>
+                      <TableCell>Username</TableCell>
+                      <TableCell>Name</TableCell>
+                      <TableCell>E-mail</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {data.obj.map(user => (
+                      <Link
+                        key={user.id}
+                        route="example.user:read"
+                        params={{ id: user.id }}
+                      >
+                        <TableRow classes={{ hover: classes.userHover }} hover>
+                          <TableCell>{user.id}</TableCell>
+                          <TableCell>{user.username}</TableCell>
+                          <TableCell>{user.full_name}</TableCell>
+                          <TableCell>{user.email}</TableCell>
+                        </TableRow>
+                      </Link>
+                    ))}
+                  </TableBody>
+                  <TableFooter>
+                    <TableRow>
+                      <TableCell>
+                        <strong>Count:</strong> {data.obj.length}
+                      </TableCell>
+                    </TableRow>
+                  </TableFooter>
+                </Table>
+              </Paper>
+            </Content>
+            <ToolBar color="paper" justify="center">
+              <Tools>
+                <Link path="/">
+                  <Button variant="contained" color="primary">
+                    Dashboard
+                  </Button>
+                </Link>
+                <Button
+                  variant="outlined"
                   color="primary"
-                  size="medium"
-                  variant="extended"
-                  className={classes.addButton}
+                  onClick={() =>
+                    context.router.route(
+                      {
+                        query: { username: "stormtrooper", age: "40" },
+                      },
+                      { patch: true }
+                    )
+                  }
                 >
-                  <PersonAddIcon className={classes.addIcon} /> Add
-                </Fab>
-              </Link>
-              <Link route="example.user:create">
-                <Fab
-                  color="secondary"
-                  size="medium"
-                  variant="extended"
-                  className={classes.addButton}
-                >
-                  <PersonAddIcon className={classes.addIcon} /> Add
-                </Fab>
-              </Link>
-              <IconButton color="inherit">
-                <Badge badgeContent={4} color="secondary">
-                  <NotificationsIcon />
-                </Badge>
-              </IconButton>
-            </Tools>
-          </TitleBar>
-          <TitleBar
-            title="Filters"
-            color="paper"
-            border="bottom"
-            dense
-            overrides={{
-              toolbar: classes.evenToolbar,
-            }}
-          >
-            <Input placeholder={"Search..."} />
-            <Tools>
-              <IconButton color="inherit">
-                <FilterListIcon />
-              </IconButton>
-            </Tools>
-          </TitleBar>
-
-          <Content contained={false}>
-            <Typography>
-              <strong>Route:</strong> {route.id}
-            </Typography>
-            <Typography>
-              <strong>Query:</strong>{" "}
-              {query &&
-                Object.entries(query).map(
-                  ([key, value]) => `${key} : ${value}, `
-                )}
-            </Typography>
-            <Typography>
-              <strong>Hash:</strong> {hash}
-            </Typography>
-            <Paper className={classes.users}>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>ID</TableCell>
-                    <TableCell>Username</TableCell>
-                    <TableCell>Name</TableCell>
-                    <TableCell>E-mail</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {data.obj.map(user => (
-                    <Link
-                      key={user.id}
-                      route="example.user:read"
-                      params={{ id: user.id }}
-                    >
-                      <TableRow classes={{ hover: classes.userHover }} hover>
-                        <TableCell>{user.id}</TableCell>
-                        <TableCell>{user.username}</TableCell>
-                        <TableCell>{user.full_name}</TableCell>
-                        <TableCell>{user.email}</TableCell>
-                      </TableRow>
-                    </Link>
-                  ))}
-                </TableBody>
-                <TableFooter>
-                  <TableRow>
-                    <TableCell>
-                      <strong>Count:</strong> {data.obj.length}
-                    </TableCell>
-                  </TableRow>
-                </TableFooter>
-              </Table>
-            </Paper>
-          </Content>
-          <ToolBar color="paper" justify="center">
-            <Tools>
-              <Link path="/">
-                <Button variant="contained" color="primary">
-                  Dashboard
+                  Query Param
                 </Button>
-              </Link>
-              <Button
-                variant="outlined"
-                color="primary"
-                onClick={() =>
-                  context.router.route(
-                    {
-                      query: { username: "stormtrooper", age: "40" },
-                    },
-                    { patch: true }
-                  )
-                }
-              >
-                Query Param
-              </Button>
 
-              <Button
-                variant="outlined"
-                color="secondary"
-                onClick={() =>
-                  context.router.route({ hash: "olof" }, { patch: true })
-                }
-              >
-                Hash
-              </Button>
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  onClick={() =>
+                    context.router.route({ hash: "olof" }, { patch: true })
+                  }
+                >
+                  Hash
+                </Button>
 
-              <Button
-                variant="outlined"
-                onClick={() =>
-                  context.router.route({ query: { foo: "bar" }, hash: "#baz" })
-                }
-              >
-                Query and Hash
-              </Button>
+                <Button
+                  variant="outlined"
+                  onClick={() =>
+                    context.router.route({
+                      query: { foo: "bar" },
+                      hash: "#baz",
+                    })
+                  }
+                >
+                  Query and Hash
+                </Button>
 
-              <Button
-                variant="outlined"
-                onClick={async () => {
-                  const me = await context.api["bananas.me:list"]();
-                  logger.info("ME", me);
-                  context.admin.info(`Fetched Me: ${me.obj.username}`);
-                }}
-              >
-                API call
-              </Button>
+                <Button
+                  variant="outlined"
+                  onClick={async () => {
+                    const me = await context.api["bananas.me:list"]();
+                    logger.info("ME", me);
+                    context.admin.info(`Fetched Me: ${me.obj.username}`);
+                  }}
+                >
+                  API call
+                </Button>
 
-              <Button
-                variant="outlined"
-                onClick={() => {
-                  context.admin.alert({
-                    title: "Do you want to do this?",
-                    message: (
-                      <React.Fragment>
-                        Will do something that can not be undone.
-                        <NotificationsIcon fontSize="small" color="secondary" />
-                      </React.Fragment>
-                    ),
-                    agree: "Okidokey!",
-                    dismiss: "Nope",
-                    onDismiss: () => context.admin.warning("Dismissed!"),
-                    onAgree: () => context.admin.success("Agreed!"),
-                  });
-                }}
-              >
-                Alert
-              </Button>
+                <Button
+                  variant="outlined"
+                  onClick={() => {
+                    context.admin.alert({
+                      title: "Do you want to do this?",
+                      message: (
+                        <React.Fragment>
+                          Will do something that can not be undone.
+                          <NotificationsIcon
+                            fontSize="small"
+                            color="secondary"
+                          />
+                        </React.Fragment>
+                      ),
+                      agree: "Okidokey!",
+                      dismiss: "Nope",
+                      onDismiss: () => context.admin.warning("Dismissed!"),
+                      onAgree: () => context.admin.success("Agreed!"),
+                    });
+                  }}
+                >
+                  Alert
+                </Button>
 
-              <Button
-                variant="outlined"
-                onClick={() => {
-                  context.admin.confirm({
-                    onDismiss: () => context.admin.warning("Dismissed!"),
-                    onAgree: () => context.admin.success("Agreed!"),
-                  });
-                }}
-              >
-                Confirm
-              </Button>
+                <Button
+                  variant="outlined"
+                  onClick={() => {
+                    context.admin.confirm({
+                      onDismiss: () => context.admin.warning("Dismissed!"),
+                      onAgree: () => context.admin.success("Agreed!"),
+                    });
+                  }}
+                >
+                  Confirm
+                </Button>
 
-              <Button variant="contained">Default</Button>
+                <Button variant="contained">Default</Button>
 
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={async () => {
-                  context.router.route("/foobar");
-                }}
-              >
-                404
-              </Button>
-            </Tools>
-          </ToolBar>
-        </>
-      )}
-    </AdminContext.Consumer>
-  );
-};
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={async () => {
+                    context.router.route("/foobar");
+                  }}
+                >
+                  404
+                </Button>
+
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => {
+                    this.setState({ throwError: true });
+                  }}
+                >
+                  500
+                </Button>
+              </Tools>
+            </ToolBar>
+          </>
+        )}
+      </AdminContext.Consumer>
+    );
+  }
+}
 
 UsersPage.propTypes = {
   route: PropTypes.object.isRequired,
