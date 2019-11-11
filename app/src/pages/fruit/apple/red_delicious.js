28cd24c2bf48f4a2eeaa96699cfa089aaeb15d48
Johan Skär Holm <johanskarholm@gmail.com>
2019-11-11 14:28:23 +0100

    Add example pages for Apple sorts

---
+++ app/src/pages/fruit/apple/red_delicious.js
@@ -0,0 +1,19 @@
+import { Typography } from "@material-ui/core";
+import { Content } from "django-bananas";
+import React from "react";
+
+export default () => (
+  <Content>
+    <Typography gutterBottom variant="h3">
+      Red delicious
+    </Typography>
+    <Typography gutterBottom>
+      The Red Delicious is a clone of apple cultigen, now comprising more than
+      50 cultivars, first recognized in Madison County, Iowa, in 1880. It is one
+      of the fifteen most popular apple cultivars in the United States,[1] but
+      has become disliked by consumers and is predicted to eventually vanish
+      from store shelves as orchards are replanted.
+    </Typography>
+    <a href="https://en.wikipedia.org/wiki/Red_Delicious">More info</a>
+  </Content>
+);
